ca952033d82edbcde7481ae4860459f1652316eb
Viktor Palmason <viktorjarlpalmason@gmail.com>
2024-01-23 11:38:46 +0100

    The score text gets updated when a block is destroyed

--- Assets/Scripts/GameStatus.cs
+++ Assets/Scripts/GameStatus.cs
@@ -1,19 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class GameStatus : MonoBehaviour
 {
     // config attributes
+
     // This manipulates the speed of the game. Its range is from 0.1 to 10. 1 is normal speed.
     [Range(0.1f, 10f)][SerializeField] float gameSpeed = 1f;
+    // Current score that the player has on a level
     [SerializeField] int currentScore = 0;
+    // The score the player gets by destroying a block
     [SerializeField] int scorePerBlockDestroyed = 10;
+
+    // Reference variables
+    [SerializeField] TextMeshProUGUI scoreText;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        scoreText.text = currentScore.ToString();   
     }
 
     // Update is called once per frame
@@ -25,5 +32,6 @@ public class GameStatus : MonoBehaviour
     public void AddPoints()
     {
         currentScore += scorePerBlockDestroyed;
+        scoreText.text = currentScore.ToString();
     }
 }
