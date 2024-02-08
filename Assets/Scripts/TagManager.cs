38953adcf9141741cb42101a63b709d8360f1ac3
Viktor Palmason <viktorjarlpalmason@gmail.com>
2024-02-08 12:19:51 +0100

    Created a public static class to store all tags used in the project.

---
+++ Assets/Scripts/TagManager.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// A static class to store all the tags used in the project
+public static class TagManager
+{
+    public const string BALL_TAG = "Ball";
+    public const string BREAKABLE = "Breakable";
+    public const string UNBREAKABLE = "Unbreakable";
+}
